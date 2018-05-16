c1ed2d08602baa346270507b2d1628d0bb4eefd4
Meike <meike_hankewicz@yahoo.de>
2018-05-16 13:33:19 +0200

    minor improvements

--- main.js
+++ main.js
@@ -4,22 +4,19 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(position) {
             var lat = position.coords.latitude;
             var long = position.coords.longitude;		  
-            var api = 'https://fcc-weather-api.glitch.me/api/current?lat='+lat+'&lon='+long;
+            var api = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+long;
 
             checkWeather(api);
         });
-
     } else {
-        document.getElementById('greeting').innerHTML = "Sorry, your browser doesn't seem to support geolocation :(";		
-    };
+        $("#greeting").html("Sorry, your browser doesn't seem to support geolocation :(");		
+    }
 });
-
 
 // Check Weather
 function checkWeather(api){
     // jQuery AJAX Request
     $.getJSON(api, function(data){
-
         // Check Weather Condition
         switch(data.weather[0].main){
             case "Clear":
@@ -41,75 +38,74 @@ function checkWeather(api){
                 break;
             case "Snow":
                 displayWeather(snow);
-        };
-
+        }
         displayTemp(data);
-    });
+    })
 };
 
 // Display Weather
 function displayWeather(object){
-    document.getElementById('icon').className = object.icon;
-    document.getElementById('activity').innerHTML = object.activity;
-    document.getElementById('description').innerHTML = object.description;
-    document.getElementById('weather').innerHTML = object.weather;
+    document.getElementById("icon").className = object.icon;
+    document.getElementById("activity").innerHTML = object.activity;
+    document.getElementById("description").innerHTML = object.description;
+    document.getElementById("weather").innerHTML = object.weather;
 };
 
 // Convert And Display Temperature
 function displayTemp(data){
-    var tempC = Math.round(data.main.temp)+ '°C.';
-    var tempF = Math.round(data.main.temp * 9 / 5 + 32)+ '°F.';
-    document.getElementById('temp').innerHTML = tempC ;
+    var tempC = Math.round(data.main.temp)+ "°C.";
+    var tempF = Math.round(data.main.temp * 9 / 5 + 32)+ "°F.";
+    document.getElementById("temp").innerHTML = tempC ;
 
     // Switch Celsius And Fahrenheit
     $("#temp").click(function(){
-        if($('#temp').text() == tempC){
-            $('#temp').text(tempF);
+        if($("#temp").text() == tempC){
+            $("#temp").text(tempF);
         } else{
-            $('#temp').text(tempC);
+            $("#temp").text(tempC);
         };
     });
 };
 
 // Description Objects
 var clear = {
-        icon: 'wi wi-day-sunny',
-        activity: 'a nice picnic',
-        description: 'be perfect',
-        weather: 'sunny'
+        icon: "wi wi-day-sunny",
+        activity: "a nice picnic",
+        description: "be perfect",
+        weather: "sunny"
     },
 
     clouds = {
-        icon: 'wi wi-day-cloudy',
-        activity: 'going for a walk',
-        description: 'be pleasant enough',
-        weather: 'a little cloudy'
+        icon: "wi wi-day-cloudy",
+        activity: "going for a walk",
+        description: "be pleasant enough",
+        weather: "a little cloudy"
     },
 
     rain = {
-        icon: 'wi wi-showers',
-        activity: 'watching a movie',
-        description: 'put you in the right mood',
-        weather: 'be rainy'
+        icon: "wi wi-showers",
+        activity: "watching a movie",
+        description: "put you in the right mood",
+        weather: "be rainy"
     },
 
     thunder = {
-        icon: 'wi wi-storm-showers',
-        activity: 'bundling up with a good book',
-        description: 'get a bit moody',
-        weather: 'stormy'
+        icon: "wi wi-storm-showers",
+        activity: "bundling up with a good book",
+        description: "get a bit moody",
+        weather: "stormy"
     },
 
     fog = {
-        icon: 'wi wi-fog',
-        activity: 'staying inside and learning a new skill',
-        description: 'get a little gloomy',
-        weather: 'foggy'
+        icon: "wi wi-fog",
+        activity: "staying inside and learning a new skill",
+        description: "get a little gloomy",
+        weather: "foggy"
     },
 
     snow = {
-        icon: 'wi wi-snow',
-        activity: 'building a snowman',
-        description: 'provide some material',
-        weather: 'snowy'
+        icon: "wi wi-snow",
+        activity: "building a snowman",
+        description: "provide some material",
+        weather: "snowy"
     };
